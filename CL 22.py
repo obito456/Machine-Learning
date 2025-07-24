af3a94859f46a067dfdb7cf688800c026129aaee
vijayuchiha <150343458+obito456@users.noreply.github.com>
2025-07-24 18:09:47 +0530

    Create CL 22.py

---
+++ CL 22.py
@@ -0,0 +1,10 @@
+# Compute mean, median, and standard deviation of a random array.
+import numpy as np
+arr=np.random.rand(10)
+meanx=np.mean(arr)
+medianx=np.median(arr)
+stdx=np.median(arr)
+print(arr)
+print(meanx)
+print(medianx)
+print(stdx)
