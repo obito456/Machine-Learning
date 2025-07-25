4e27333ef9cfcb2e8c7aeef427a6810751bad9b9
vijayuchiha <150343458+obito456@users.noreply.github.com>
2025-07-25 20:31:18 +0530

    Create Msize 2.py

---
+++ Msize 2.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+a=np.array([1,2,3,4,5,6,7,8,9,10])
+b=np.array([10,50,25,35,45,16,40,60,38,22])
+size=np.array([10,20,30,40,50,60,70,180,10,40])
+plt.scatter(a,b,s=size,alpha=0.3)
+plt.show()
