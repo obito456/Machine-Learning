81a1fe9c2dba506f8abbfb90b4935eb17c91ac28
vijayuchiha <150343458+obito456@users.noreply.github.com>
2025-07-25 18:21:57 +0530

    Create Mplot 6.py

---
+++ Mplot 6.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+a=np.array([2,4,6,8,10])
+b=np.array([5,10,5,10,6])
+c=np.array([5,10,5,10,6])
+d=np.array([2,4,6,8,10])
+plt.plot(a,b,c,d)
+plt.show()
