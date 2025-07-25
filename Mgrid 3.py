9686c830a2e298e4659329ca63b3fee3175b0540
vijayuchiha <150343458+obito456@users.noreply.github.com>
2025-07-25 18:50:58 +0530

    Create Mgrid 3.py

---
+++ Mgrid 3.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+x=np.array([1,2,3])
+y=np.array([140,240,420])
+plt.plot(x,y)
+plt.xlabel("Quantity")
+plt.ylabel("price")
+plt.title("Restaurant")
+plt.grid(axis='y',color='hotpink',linestyle=":",linewidth=5)
+plt.show()
