1ca6aeb31e5b60c0331e40c1148353a5ba1145af
vijayuchiha <150343458+obito456@users.noreply.github.com>
2025-07-25 19:24:57 +0530

    Create Mscatter 2.py

---
+++ Mscatter 2.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+a=np.array([1,2,3])
+b=np.array([1,20,30])
+c=np.array([1,20,30])
+d=np.array([1,2,3])
+plt.scatter(a,b)
+plt.scatter(c,d)
+plt.show()
