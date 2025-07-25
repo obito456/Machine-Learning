eceb3ad5117ed0b59a501aea849b4fd2eb9eb96d
vijayuchiha <150343458+obito456@users.noreply.github.com>
2025-07-25 19:00:39 +0530

    Create Msubplot 2.py

---
+++ Msubplot 2.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+a=np.array([1,2,3])
+b=np.array([140,240,420])
+c=np.array([10,20,30])
+d=np.array([5,7,20])
+plt.subplot(2,1,2)
+plt.plot(a,b)
+plt.subplot(2,1,1)
+plt.plot(c,d)
+plt.show()
