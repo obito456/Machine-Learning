0d79b99f20d5d88b78afb28670c4b89918aa3252
vijayuchiha <150343458+obito456@users.noreply.github.com>
2025-07-26 18:14:43 +0530

    Create Mpie 5.py

---
+++ Mpie 5.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+x=["Civil","ECE","CSE"]
+y=np.array([25,35,40])
+color=['yellow','red','pink']
+plt.pie(y,labels=x,shadow=True,colors=color)
+plt.legend()
+plt.show()
